5b9937e0fbe5c225f5923c0774b5e57eba0d9bcc
jordantangy <49556564+jordantangy@users.noreply.github.com>
2022-04-05 18:50:59 +0300

    fix screen and button desing

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -27,7 +27,7 @@ const Calculator = (props) => {
   };
 
   const cHandler = () => {
-    setout(0);
+    setout("");
     setExpr("");
     setResult(0);
   };
@@ -37,78 +37,78 @@ const Calculator = (props) => {
       <Screen val={out} res={result}></Screen>
       <Button
         value={0}
-        style={{ left: 580, bottom: 140 }}
+        style={{ left: 10, top: 330 }}
         exprHandler={onTypeHandler}
       />
       <Button
         value={1}
-        style={{ left: 580, top: 400 }}
+        style={{ left: 10, top: 160 }}
         exprHandler={onTypeHandler}
       />
       <Button
         value={2}
-        style={{ left: 680, top: 400 }}
+        style={{ left: 110, top: 80 }}
         exprHandler={onTypeHandler}
       />
       <Button
         value={3}
-        style={{ left: 780, top: 400 }}
+        style={{ left: 210, top: 0 }}
         exprHandler={onTypeHandler}
       />
       <Button
         value={4}
-        style={{ left: 580, bottom: 330 }}
+        style={{ left: 10, top: -170 }}
         exprHandler={onTypeHandler}
       />
       <Button
         value={5}
-        style={{ left: 680, bottom: 330 }}
+        style={{ left: 110, top: -250 }}
         exprHandler={onTypeHandler}
       />
       <Button
         value={6}
-        style={{ left: 780, bottom: 330 }}
+        style={{ left: 210, top: -330 }}
         exprHandler={onTypeHandler}
       />
       <Button
         value={7}
-        style={{ left: 580, bottom: 420 }}
+        style={{ left: 10, top: -500 }}
         exprHandler={onTypeHandler}
       />
       <Button
         value={8}
-        style={{ left: 680, bottom: 420 }}
+        style={{ left: 110, top: -580 }}
         exprHandler={onTypeHandler}
       />
       <Button
         value={9}
-        style={{ left: 780, bottom: 420 }}
+        style={{ left: 210, top: -660 }}
         exprHandler={onTypeHandler}
       />
       <Button
         value={"x"}
-        style={{ left: 880, bottom: 420 }}
+        style={{ left: 310, top: -740 }}
         exprHandler={onTypeHandler}
       />
       <Button
         value={"-"}
-        style={{ left: 880, bottom: 330 }}
+        style={{ left: 310, top: -730 }}
         exprHandler={onTypeHandler}
       />
       <Button
         value={"+"}
-        style={{ left: 880, bottom: 235 }}
+        style={{ left: 310, top: -720 }}
         exprHandler={onTypeHandler}
       />
       <Button
         value={"="}
-        style={{ left: 880, bottom: 140 }}
+        style={{ left: 310, top: -710 }}
         exprHandler={eqHandler}
       />
-      <Button value={"."} style={{ left: 780, bottom: 140 }} />
+      <Button value={"."} style={{ left: 210, top: -790 }} />
       <Button
         value="C"
-        style={{ left: 680, bottom: 140 }}
+        style={{ left: 110, top: -870 }}
         exprHandler={cHandler}
       />
     </div>
