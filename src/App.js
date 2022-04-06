60cde64bf4efa8c1ad3d2226aadb403fc007a6c1
jordantangy <49556564+jordantangy@users.noreply.github.com>
2022-04-07 01:39:21 +0300

    adding styles

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Calculator from "./components/calculator";
 import "./components/calc.css";
 import "./components/Button.css";
 import Screen from "./components/Screen";
+import Style from "./components/Style";
 
 function App() {
   return (
     <div>
       <Calculator />
+      <Style />
     </div>
   );
 }
---
+++ src/components/Style.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+const Style = () => {
+  return (
+    <div class="loader-wrapper">
+      <div class="loader">
+        <div class="loader loader-inner">n</div>
+      </div>
+    </div>
+  );
+};
+
+export default Style;
